b3fa7cf7d69a656f3b6f88a670e6ea5167f42813
Kelly <kellybhoward@gmail.com>
2015-12-05 00:02:50 -0800

    fixed formatting to JS

--- radix_sort.js
+++ radix_sort.js
@@ -1,34 +1,26 @@
-function radixSort(array)
-{
+function radixSort(array){
     var count = 0;
     var sorted = false;
-    while(!sorted)
-    {
+    while(!sorted){
         var map = {0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[]}; //bucket to collect numbers
         var size = array.length;
-        for( var i = 0 ; i < size ; i ++)
-        {
+        for( var i = 0 ; i < size ; i ++){
             var word = array[i].toString().split(""); //make 'word' array of individual numbers
-            if( word.length <= count ) 
-            {
+            if( word.length <= count ){
                 map[0].push(array[i]); //if decimal place is 0
-            } else
-            {
+            } else{
                 map[word[(word.length)-(1)- count]].push(array[i]);
             }
             
         }
         var collector = [];
-        for ( var index = 0 ; index <= 9 ; index ++ )
-        {
-          for ( var valueI = 0 ; valueI < map[index].length ; valueI++ )
-          {
+        for ( var index = 0 ; index <= 9 ; index ++ ){
+          for ( var valueI = 0 ; valueI < map[index].length ; valueI++ ){
             collector.push(map[index][valueI]);
           }
         }
         array = collector;
-        if(map[0].length == array.length) 
-        {
+        if(map[0].length == array.length) {
           sorted = true;
         }
         count++;
